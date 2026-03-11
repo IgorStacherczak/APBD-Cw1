69680d26a9d8919616391d5efa4bd686d4bb2816
s34077 <s34077@pjwstk.edu.pl>
2026-03-11 09:55:42 +0100

    Napisanie Szkieletu Klasy

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,6 +4,16 @@ using System;
 
 static int CalculateMin(int[] values)
 {
+    if (values == null || values.Length == 0)
+        throw new ArgumentException("Tablica jest pusta lub null");
 
-    return 0;
+    int min = values[0];
+
+    foreach (int v in values)
+    {
+        if (v < min)
+            min = v;
+    }
+
+    return min;
 }
