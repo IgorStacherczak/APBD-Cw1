82850777361bbf5dde051e06dd050fbc61d8d9cc
s34077 <s34077@pjwstk.edu.pl>
2026-03-11 09:24:20 +0100

    Zmiana z napisu Hello World na Podaj liczbę. Dodanie możliwości wbicia wartośći zmiennej z klawiatury.Dodanie else gdy liczba jest nie parzysta dostaniemy komunikat

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Podaj Liczbę: ");
 
-int a = 2;
+int a = Convert.ToInt32(Console.ReadLine());
 
 if (a % 2 == 0){
-    Console.WriteLine("Liczba jest parzysta" + a);
+    Console.WriteLine("Liczba jest parzysta " + a);
 }
+else
+{
+    Console.WriteLine("Liczba jest nie parzysta " + a);
+}
+
